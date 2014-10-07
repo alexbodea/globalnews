04b5c35ac600dd7b97acdf4921e97f63c0320bad
Claudiu Limban <sofuxro@yahoo.com>
2014-10-07 13:42:52 +0300

    front-end Production ready !

--- public_html/controllers/includes/header.php
+++ public_html/controllers/includes/header.php
@@ -1,25 +1,23 @@
 <?php
 
-    $home =  translate('Home',$countryslug);
     $country = translate('Country',$countryslug);
-    $about = translate('About',$countryslug);
 
-    $sql = "SELECT country2,slug FROM slugs ORDER BY CASE 
-                                                    WHEN slug='$countryslug' THEN 1 
+    $sql = "SELECT country2,slug FROM slugs ORDER BY CASE
+                                                    WHEN slug='$countryslug' THEN 1
                                                     ELSE country2
                                                 END ASC";
-                              
+
     $res = mysqli_query($con,$sql);
     $array_index = 1;
     $countrys = [];
     while($row = mysqli_fetch_array($res)) {
 
         $countrys[$array_index]['country'] = $row['country2'];
-        $countrys[$array_index]['slug'] = $row['slug'];
+        $countrys[$array_index]['slug']    = $row['slug'];
         $array_index = $array_index+1;
     }
 
 
-    echo $twig->render('header.html', array('theme' => $active_theme , 'home' => $home , 'country' => $country, 'about' => $about , 'countrys' => $countrys ));
+    echo $twig->render('header.html', array('theme' => $active_theme, 'country' => $country, 'countrys' => $countrys));
 
 ?>
